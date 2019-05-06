1dad20d5785df3e4cddd72efe102962ac4441191
vladplesu <vladp1987@gmail.com>
2019-05-06 16:35:41 +0300

    added validation for input file form added debugger for chrome

--- www/js/upload-scripts.js
+++ www/js/upload-scripts.js
@@ -1,7 +1,24 @@
-const inputFile = $('#inputGroup');
-const inputFileLbl = $('label[for="inputGroup"]');
+const $inputFile = $('#inputGroup');
+const $inputFileLbl = $('label[for="inputGroup"]');
+const $loadBtn = $('button[type="submit"]');
+const $invalidFile = $('div.invalid-feedback');
 
-inputFile.change(function() {
-    const fileName = inputFile.prop('files');
-    inputFileLbl.prop('innerText', fileName[0].name);
+$inputFile.change(function() {
+    const fileName = $inputFile[0].files[0].name;
+
+    const reg = /\.ifc$/;
+    const isFileIFC = reg.test(fileName);
+
+    $inputFileLbl[0].innerText = fileName;
+    if (!isFileIFC) {
+        $inputFile[0].classList.add('is-invalid');
+        $invalidFile.slideDown('slow');
+        return;
+    }
+    $inputFile[0].classList.add('is-valid');
+    $inputFile[0].classList.remove('is-invalid');
+    $loadBtn[0].disabled = false;
+    $loadBtn[0].classList.add('btn-primary');
+    $loadBtn[0].classList.remove('btn-secondary');
+    $invalidFile.slideUp('slow');
 });
